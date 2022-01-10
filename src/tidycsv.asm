4af06a79a0cdfde89e07ccde69602914e54a2b68
Zachary Westerman <westerman.zachary@gmail.com>
2022-01-09 19:59:24 -0600

    remove CR characters from output

--- src/tidycsv.asm
+++ src/tidycsv.asm
@@ -11,12 +11,29 @@ section .data
 	help_msg: db "Usage: tidycsv {input_file} {output_file}", 0x0a
 	help_msg_len: equ $ - help_msg
 
+	progress: db "|/-\"
+	rst: db 0x0c
+
+	delim: equ ','
+
 section .bss
 	input_file: resq 1
 	output_file: resq 1
+	is_quoted: resq 1
+	is_escaped: resq 1
+
+	output_buffer: resb 4096
+
+	chunk_number: resq 1
 
 section .text
 _start:
+	;setup data
+	xor rax, rax
+	mov [is_quoted], rax
+	mov [is_escaped], rax
+
+	; check argc
 	pop rax ;argc
 	cmp rax, 2
 	jg _open_files
@@ -58,11 +75,35 @@ _start:
 		cmp rbx, 0
 		je _done_printing
 
+		mov rcx, 0
+		_lp:
+			cmp rbx, 0
+			jz _lp_end
+
+			mov dh, [rax, rcx]
+
+			; cmp dh, delim
+			; jz _next_lp
+			cmp dh, 0x0d
+			jz _next_lp
+
+			_append_to_output:
+			mov [output_buffer, rcx], dh
+
+			add rcx, 1
+			_next_lp:
+			sub rbx, 1
+			jmp _lp
+		_lp_end:
+
 		; output to file
+		mov rax, output_buffer
+		mov rbx, rcx
+		; mov rdi, io.stdout
 		mov rdi, [output_file]
 		call io.write
 
-
+		; TEMP: don't loop again
 		jmp _print_contents
 	_done_printing:
 
