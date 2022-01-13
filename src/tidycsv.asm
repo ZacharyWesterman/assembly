6d399433fd97a59df9ec0749f77d8806a2e5c6b2
Zachary Westerman <westerman.zachary@gmail.com>
2022-01-12 21:52:21 -0600

    replace all crlf with cr

--- src/tidycsv.asm
+++ src/tidycsv.asm
@@ -21,6 +21,7 @@ section .bss
 	output_file: resq 1
 	is_quoted: resq 1
 	is_escaped: resq 1
+	found_cr: resq 1
 
 	output_buffer: resb 4096
 
@@ -32,6 +33,7 @@ _start:
 	xor rax, rax
 	mov [is_quoted], rax
 	mov [is_escaped], rax
+	mov [found_cr], rax
 
 	; check argc
 	pop rax ;argc
@@ -80,19 +82,37 @@ _start:
 			cmp rbx, 0
 			jz _lp_end
 
-			mov dh, [rax, rcx]
+			xor rdx, rdx
+			mov dl, [rax] ;dh contains this character
+			add rax, 1
+			push rax
 
-			; cmp dh, delim
-			; jz _next_lp
-			cmp dh, 0x0d
-			jz _next_lp
+
+			; if this character is LF and last char was CR
+			mov rax, [found_cr]
+			add rax, rdx
+			cmp rax, 0x0b
+			jnz _after_chk_crlf
+			;then set prev char to this one, and skip
+			sub rcx, 1
+			_after_chk_crlf:
+
+			; if this character is CR,
+			; make a note of that
+			xor rax, rax
+			cmp dl, 0x0d
+			jnz _cif_ret ; if this char != '\r'
+			mov rax, 1
+			_cif_ret:
+			mov [found_cr], rax
 
 			_append_to_output:
-			mov [output_buffer, rcx], dh
+			mov [output_buffer, rcx], dl
 
 			add rcx, 1
 			_next_lp:
 			sub rbx, 1
+			pop rax
 			jmp _lp
 		_lp_end:
 
